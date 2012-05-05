9a7680a3bfad077d3233c8f890db6bd5e003752d
Pimm Hogeling <pimmhogeling@gmail.com>
2012-05-05 22:55:47 +0200

    Updated the tests to match the rewrite

--- test/test.js
+++ test/test.js
@@ -1,193 +1,125 @@
-/**
- * EventEmitter tests
- */
-
-(function() {
-	test('Adding and retrieving listeners', function() {
-		var ee = new EventEmitter();
-		
-		equal(ee.listeners('testEvent').length, 0, 'Checking for any listeners');
-		
-		ee.addListener('testEvent', function() {
-			// Listener
-			return true;
-		});
-		equal(ee.listeners('testEvent')[0](), true, 'Retrieving the first listener');
-		
-		ee.addListener('testEvent', function() {
-			// Another listener for the same event
-			return true;
-		});
-		equal(ee.listeners('testEvent')[1](), true, 'Retrieving the second listener');
-		
-		ee.addListener('anotherTestEvent', function() {
-			// Listener for new event
-			return true;
-		});
-		equal(ee.listeners('anotherTestEvent')[0](), true, 'Retrieving the first listener for a different event');
-		
-		ee.addListener('anotherTestEvent', function() {
-			// Another listener for new event
-			return true;
-		});
-		equal(ee.listeners('anotherTestEvent')[1](), true, 'Retrieving the second listener for a different event');
-		
-		ee.on('onTest', function() {
-			// Listener via the alias
-			return true;
-		});
-		equal(ee.listeners('onTest')[0](), true, 'Retrieving the first listener for a different event added via the on alias');
-		
-		var scopeTarget = {
-			foo: false
-		};
-		ee.addListener('scopeTest', function() {
-			// Scope test
-			this.foo = true;
-		}, scopeTarget);
-		equal(scopeTarget.foo, false, 'Check that the scope target is currently false');
-		ee.emit('scopeTest');
-		equal(scopeTarget.foo, true, 'Check that the scope target has been changed to true');
+"use strict";
+test("Add and emit test", function() {
+	var dog = {};
+	dog.eventEmitter = new EventEmitter(dog);
+	var barked = false;
+	dog.eventEmitter.add("bark", function() {
+		barked = true;
 	});
-	
-	test('Removing listeners', function() {
-		var ee = new EventEmitter();
-		
-		function testListener() {
-			// Listener
-			return true;
-		}
-		
-		function testListener2() {
-			// Listener 2
-			return true;
-		}
-		
-		ee.addListener('testEvent', testListener);
-		equal(ee.listeners('testEvent')[0](), true, 'Retrieving the first listener');
-		
-		ee.addListener('testEvent', testListener2);
-		equal(ee.listeners('testEvent')[1](), true, 'Retrieving the second listener');
-		
-		ee.removeListener('testEvent', testListener);
-		equal(ee.listeners('testEvent')[1], undefined, 'Retrieving the second listener (should be gone)');
-		equal(ee.listeners('testEvent')[0], testListener2, 'First should now be the second');
-		
-		ee.removeListener('testEvent', testListener2);
-		ok(ee.listeners('testEvent'), 'Retrieving any listeners (should be gone)');
-		
-		ee.addListener('removeAllTest', function() {
-			// Listener
-			return true;
-		});
-		ee.addListener('removeAllTest', function() {
-			// Another listener for the same event
-			return true;
-		});
-		equal(ee.listeners('removeAllTest')[1](), true, 'Check for a second removeAllTest listener');
-		ee.removeAllListeners('removeAllTest');
-		equal(ee.listeners('removeAllTest').length, 0, 'Retrieving any listeners from removeAllTest (should be gone)');
-		
-		var scopeTarget = {
-			foo: false
-		};
-		ee.addListener('scopeTest', testListener);
-		ee.addListener('scopeTest', testListener);
-		ee.addListener('scopeTest', testListener, scopeTarget);
-		ee.addListener('scopeTest', testListener);
-		equal(ee._events['scopeTest'][2].scope, scopeTarget, 'Check that the one with the scope exists.');
-		equal(ee.listeners('scopeTest')[2](), true, 'Make sure we can execute the scoped listener.')
-		ee.removeListener('scopeTest', testListener, scopeTarget);
-		equal(ee._events['scopeTest'][0].scope, undefined, 'The last three should have no scope. (1)');
-		equal(ee._events['scopeTest'][1].scope, undefined, 'The last three should have no scope. (2)');
-		equal(ee._events['scopeTest'][2].scope, undefined, 'The last three should have no scope. (3)');
+	dog.eventEmitter.emit("bark");
+	strictEqual(barked, true);
+});
+test("Listener scope test", function() {
+	var dog = {};
+	dog.eventEmitter = new EventEmitter(dog);
+	// Use a new object as the initial value, to make sure no value can equal it.
+	var firstScope = {};
+	dog.eventEmitter.add("bark", function() {
+		firstScope = this;
 	});
-	
-	test('Removing all listeners', function() {
-		var ee = new EventEmitter();
-		
-		ee.on('foo', function() {
-			console.log('bar');
-		});
-		
-		equal(ee.listeners('foo').length, 1, 'Check that it was added');
-		
-		ee.removeAllListeners();
-		
-		equal(ee.listeners('foo'), false, 'Check that it was removed');
+	var secondScope = {};
+	var desiredSecondScope = {};
+	dog.eventEmitter.add("bark", function() {
+		secondScope = this;
+	}, desiredSecondScope);
+	dog.eventEmitter.emit("bark");
+	ok(firstScope === dog);
+	ok(secondScope === desiredSecondScope);
+	var anonymousEventEmitter = new EventEmitter();
+	var thirdScope = {};
+	anonymousEventEmitter.add("emit", function() {
+		thirdScope = this;
 	});
-	
-	test('Emitting events', function() {
-		var ee = new EventEmitter();
-		
-		ee.addListener('emittingTest', function() {
-			// Listener
-			ok(true, 'First called');
-		});
-		ee.addListener('emittingTest', function() {
-			// Another listener for the same event
-			ok(true, 'Second called');
-		});
-		ee.addListener('differentEvent', function() {
-			// Another listener for the same event
-			ok(false, 'Wrong event called');
-		});
-		ee.emit('emittingTest');
+	anonymousEventEmitter.emit("emit");
+	ok(thirdScope === anonymousEventEmitter);
+});
+test("Bond destroying", function() {
+	var dog = {};
+	dog.eventEmitter = new EventEmitter(dog);
+	var barkCount = 0;
+	var bond = dog.eventEmitter.add("bark", function() {
+		barkCount++;
 	});
-	
-	test('Adding and calling a once event', function() {
-		var ee = new EventEmitter();
-		
-		function normalFunction() {
-			// Another listener for the same event but not a once event
-			return true;
-		}
-		
-		ee.once('onceTest', function() {
-			// Listener
-			return true;
-		});
-		ee.once('onceTest', function() {
-			// Another listener for the same event
-			return true;
-		});
-		ee.addListener('onceTest', normalFunction);
-		equal(ee.listeners('onceTest')[2](), true, 'Check for a third onceTest listener');
-		
-		// Emit the once test event
-		ee.emit('onceTest');
-		equal(ee.listeners('onceTest')[0], normalFunction, 'There should only be one survivor, the normalFunction one');
-		equal(ee.listeners('onceTest')[2], undefined, 'And a second should be undefined');
+	dog.eventEmitter.emit("bark");
+	bond.destroy();
+	dog.eventEmitter.emit("bark");
+	// Call destroy again: calling it twice makes no sense, but it shouldn't be a problem either.
+	bond.destroy();
+	strictEqual(barkCount, 1);
+});
+test("Bond destroying on use", function() {
+	var dog = {};
+	dog.eventEmitter = new EventEmitter(dog);
+	var barkCount = 0;
+	dog.eventEmitter.add("bark", function() {
+		barkCount++;
+	}).destroyOnUse();
+	dog.eventEmitter.emit("bark");
+	dog.eventEmitter.emit("bark");
+	strictEqual(barkCount, 1);
+	var bond = dog.eventEmitter.add("bark", function() {
+		barkCount++;
+	}).destroyOnUse();
+	// Call destroyOnUse again: calling it twice makes no sense, but it shouldn't be a problem either.
+	bond.destroyOnUse();
+	dog.eventEmitter.emit("bark");
+	dog.eventEmitter.emit("bark");
+	strictEqual(barkCount, 2);
+});
+test("Reserved event types", function() {
+	var dog = {};
+	dog.eventEmitter = new EventEmitter(dog);
+	var watched = false;
+	// This line shouldn't cause anything out of the ordinary.
+	dog.eventEmitter.emit("watch");
+	dog.eventEmitter.add("watch", function() {
+		watched = true;
 	});
-	
-	test('Passing arguments to listeners', function() {
-		var ee = new EventEmitter();
-		
-		ee.addListener('argTest', function(aBool) {
-			equal(aBool, true, 'Passing of a boolean that equals true');
-		});
-		
-		ee.addListener('argTest2', function(aString, aBool) {
-			equal(aString, 'foo', 'Passing of a string');
-			equal(aBool, true, 'Passing of a boolean that equals true as the second argument');
-		});
-		
-		ee.emit('argTest', true);
-		ee.emit('argTest2', 'foo', true);
+	dog.eventEmitter.emit("watch");
+	strictEqual(watched, true);
+	dog = {};
+	dog.eventEmitter = new EventEmitter(dog);
+	var barked = false;
+	var barkSound = "toString";
+	// This line shouldn't cause anything out of the ordinary.
+	dog.eventEmitter.emit(barkSound);
+	dog.eventEmitter.add(barkSound, function() {
+		barked = true;
 	});
-	
-	test('Exceeding the max listener count', function() {
-		var i = null;
-		
-		var ee = new EventEmitter();
-		ee.setMaxListeners(3);
-		
-		for(i = 0; i < 4; i += 1) {
-			ee.on('foo', function() {
-				console.log('bar');
-			});
-		}
-		
-		equal(ee._events.foo.warned, true, 'Check if we have been warned');
+	dog.eventEmitter.emit(barkSound);
+	ok(barked);
+});
+test("Add, emit and remove test", function() {
+	var dog = {};
+	dog.eventEmitter = new EventEmitter(dog);
+	var barkCount = 0;
+	function increaseBarkCount() {
+		barkCount++;
+	}
+	dog.eventEmitter.add("bark", increaseBarkCount);
+	dog.eventEmitter.emit("bark");
+	dog.eventEmitter.remove("bark", increaseBarkCount);
+	dog.eventEmitter.emit("bark");
+	strictEqual(barkCount, 1);
+});
+test("Add, emit and remove with clutter test", function() {
+	var dog = {};
+	dog.eventEmitter = new EventEmitter(dog);
+	var barkCount = 0;
+	function increaseBarkCount() {
+		barkCount++;
+	}
+	function doNothing() {
+	}
+	dog.eventEmitter.add("bark", function() {
 	});
-}());
+	dog.eventEmitter.add("bark", increaseBarkCount);
+	dog.eventEmitter.add("bark", doNothing);
+	dog.eventEmitter.add("bark", function() {
+	});
+	dog.eventEmitter.emit("bark");
+	dog.eventEmitter.remove("bark", doNothing);
+	dog.eventEmitter.remove("bark", increaseBarkCount);
+	dog.eventEmitter.emit("bark");
+	strictEqual(barkCount, 1);
+});
