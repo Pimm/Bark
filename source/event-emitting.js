55df33e5142ad79915e9f4f2775e55bef144f4f8
Pimm Hogeling <pimmhogeling@gmail.com>
2012-05-05 22:55:35 +0200

    Rewrote the event emitter

--- source/event-emitting.js
+++ source/event-emitting.js
@@ -1,7 +1,4 @@
 /**
- * EventEmitter v3.1.4
- * https://github.com/Wolfy87/EventEmitter
- *
  * Copyright 2012 Pimm Hogeling, Oliver Caldwell (olivercaldwell.co.uk)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
@@ -21,277 +18,294 @@
  * later (the "GPL"), or the GNU Lesser General Public License Version 3 or later (the "LGPL"), in which case the provisions of
  * the GPL or the LGPL are applicable instead of those above.
  */
+"use strict";
+/**
+ * Whether Bark should support Internet Explorer 8.
+ *
+ * @define {boolean}
+ */
+var jurassic = true;
 (function(exports) {
-	// JSHint config
-	/*global define:true*/
-	
-	// Place the script into strict mode
-	'use strict';
-	
 	/**
-	 * EventEmitter class
-	 * Creates an object with event registering and firing methods
+	 * An empty object, which will be used to determine which event types cannot be directly used.
+	 * As objects might have a "watch" property by default, for instance, we should not use "watch" event types in such case.
+	 * Event types that equal the names of properties that exist in object by default are referred to as "reserved" event types,
+	 * and they will be prefixed.
+	 *
+	 * @const
+	 * @type {!Object}
 	 */
-	function EventEmitter() {
-		// Initialise required storage variables
-		this._events = {};
-		this._maxListeners = 10;
+	var emptyObject = {};
+	/**
+	 * The prefix that will be prepended (or shoud I say, unshifted) to "reserved" event types.
+	 *
+	 * @const
+	 * @type {!string}
+	 */
+	var reservedEventTypePrefix = "event-type-";
+	/**
+	 * A null-object function.
+	 *
+	 * @return {undefined}
+	 */
+	function nop() {
 	}
-	
 	/**
-	 * Event class
-	 * Contains Event methods and property storage
-	 * 
-	 * @param {String} type Event type name
-	 * @param {Function} listener Function to be called when the event is fired
-	 * @param {Object} scope Object that this should be set to when the listener is called
-	 * @param {Boolean} once If true then the listener will be removed after the first call
-	 * @param {Object} instance The parent EventEmitter instance
+	 * An event emitter is an object one can register event listeners to. 
+	 *
+	 * @constructor
+	 * @param {*=} targetAndDefaultScope
 	 */
-	function Event(type, listener, scope, once, instance) {
-		// Store arguments
-		this.type = type;
+	function EventEmitter(targetAndDefaultScope) {
+		this.bondBundles = {};
+		this.targetAndDefaultScope = undefined === targetAndDefaultScope ? this : targetAndDefaultScope;
+	}
+	if (jurassic) {
+		/**
+		 * Returns a version of the passed listener that is bound to the passed scope. (Jurassic Bark only.)
+		 *
+		 * @param {!function():*} listener
+		 * @param {*} scope
+		 * @return {!function():*}
+		 */
+		var bindListener = (function() {
+			// If the engine supports Function.prototype.bind, use it.
+			if (nop.bind) {
+				return function(listener, scope) {
+					return listener.bind(scope);
+				};
+			// If the engine does not support Function.prototype.bind, use this custom implementation.
+			} else {
+				return function(listener, scope) {
+					return function() {
+						listener.apply(scope, arguments);
+					};
+				};
+			}
+		})();
+	}
+	/**
+	 * A bond between an event emitter and a listener.
+	 * Contract: after creating a bond, you must set its bonds property.
+	 *
+	 * @constructor
+	 * @param {!function():*} listener
+	 * @param {*} scope
+	 */
+	function Bond(listener, scope) {
 		this.listener = listener;
-		this.scope = scope;
-		this.once = once;
-		this.instance = instance;
+		this.originalScope = scope;
+		// Define a callListener property, which is a callListener property that respects the scope.
+		if (null === scope) {
+			this.callListener = listener;
+		} else {
+			this.callListener = jurassic ? bindListener(listener, scope) : listener.bind(scope);
+		}
 	}
-	
+	if (jurassic) {
+		/**
+		 * Returns the last index at which a given element can be found in the array, or -1 if it is not present. The array is
+		 * searched backwards. (Jurassic Bark only.)
+		 *
+		 * @param {!Array} array
+		 * @param {*} searchElement
+		 * @return {number}
+		 */
+		var find = (function() {
+			// If the engine supports Array.prototype.lastIndexOf, use it.
+			if (false == jurassic || [].lastIndexOf) {
+				return function(array, searchElement) {
+					return array.lastIndexOf(searchElement);
+				};
+			// If the engine does not support Array.prototype.lastIndexOf, use this custom implementation.
+			} else {
+				return function(array, searchElement) {
+					for (var index = array.length; index != -1; index--) {
+						if (array[index] === searchElement) {
+							return index;
+						}
+					}
+					return -1;
+				};
+			}
+		})();
+	}
 	/**
-	 * Executes the listener
-	 * 
-	 * @param {Array} args List of arguments to pass to the listener
-	 * @return {Boolean} If false then it was a once event
+	 * Destroys the bond. The event emitter will no longer notify the listener. Bonds cannot be "undestroyed".
+	 *
+	 * @return {undefined}
 	 */
-	Event.prototype.fire = function(args) {
-		this.listener.apply(this.scope || this.instance, args);
-		
-		// Remove the listener if this is a once only listener
-		if(this.once) {
-			this.instance.removeListener(this.type, this.listener, this.scope);
-			return false;
+	Bond.prototype["destroy"] = function() {
+		var index = jurassic ? find(this.bonds, this) : this.bonds.lastIndexOf(this);
+		if (-1 != index) {
+			this.bonds.splice(index, 1);
+			// Delete the data inside the bond.
+			delete this.listener;
+			delete this.originalScope;
+			delete this.callListener;
+			delete this.bonds;
+			// Overwrite the destroy method, so calling it again will have no effect.
+			this["destroy"] = nop;
 		}
 	};
-	
 	/**
-	 * Passes every listener for a specified event to a function one at a time
-	 * 
-	 * @param {String} type Event type name
-	 * @param {Function} callback Function to pass each listener to
-	 * @return {Object} The current EventEmitter instance to allow chaining
+	 * Destroys the bond right after the event emitter notifies the listener. Whether the event emitter has already notified the
+	 * listener at the moment this method is called does not matter: the bond will be destroyed on its first use after this
+	 * method has been called. This method returns this bond itself.
+	 *
+	 * @return {!Bond}
 	 */
-	EventEmitter.prototype.eachListener = function(type, callback) {
-		// Initialise variables
-		var i = null,
-			possibleListeners = null,
-			result = null;
-		
-		// Only loop if the type exists
-		if(this._events.hasOwnProperty(type)) {
-			possibleListeners = this._events[type];
-			
-			for(i = 0; i < possibleListeners.length; i += 1) {
-				result = callback.call(this, possibleListeners[i], i);
-				
-				if(result === false) {
-					i -= 1;
+	Bond.prototype["destroyOnUse"] = function() {
+		var bond = this;
+		var oldCallListener = this.callListener;
+		this.callListener = function() {
+			oldCallListener.call(this);
+			bond["destroy"]();
+		};
+		return bond;
+	};
+	/**
+	 * Registers the passed listener as a listener for the passed event type, so the passed listener will be notified when the
+	 * event emitter emits an event with the passed event type.
+	 *
+	 * Returns the bond between the event emitter and the listener. The bond can be destroyed by calling either the remove method
+	 * of the event emitter, or the destroy method of the returned bond.
+	 *
+	 * The passed scope will be used as the default scope for the listeners that will be registered to this event emitter. If no
+	 * scope is passed, the event emitter itself will be the default scope.
+	 *
+	 * If a listener is registered to an event emitter while it is processing/emitting an event, that listener will not be
+	 * notified of that event.
+	 *
+	 * If multiple identical listeners are registered to the same event emitter with the same scope, the duplicates are
+	 * discarded. They do not cause the listener to be notified of the same event twice, and since the duplicates are discarded,
+	 * they do not need to be removed manually. When trying to register a diplucate, the bond returned while registering it the
+	 * first time will be returned.
+	 *
+	 * @param {!string} eventType
+	 * @param {function():*} listener
+	 * @param {*=} scope
+	 * @return {!Bond}
+	 */
+	EventEmitter.prototype["add"] = function(eventType, listener, scope) {
+		// Determine the scope that will be passed to the bond, based on the passed scope and the target passed to the constructor.
+		if (undefined === scope) {
+			// If no specific scope is defined, not in the constructor nor in this function, use null as the scope.
+			if (this == this.targetAndDefaultScope) {
+				scope = null;
+			// If no scope was defined to in this function, but a scope was defined in the constructor, use that scope.
+			} else {
+				scope = this.targetAndDefaultScope;
+			}
+		}
+		// Find the bond bundle. Prefix "reserved" event types.
+		if (emptyObject[eventType]) {
+			var bonds = this.bondBundles[eventType = (reservedEventTypePrefix + eventType)];
+		} else {
+			var bonds = this.bondBundles[eventType];
+		}
+		var bond;
+		// Create the bond bundle if it doesn't exist yet, and push a new bond to it.
+		if (!bonds) {
+			bonds = this.bondBundles[eventType] = [bond = new Bond(listener, scope)];
+		// Push a new bond to the bond bundle, if such a bond bundle existed already.
+		} else {
+			// However, if a bond with the passed listener and scope already exists, return it. Don't add a new bond.
+			for (var index = 0; index < bonds.length; index++) {
+				if (bonds[index].listener == listener) {
+					bond = bonds[index];
+					if (bond.originalScope === scope) {
+						return bond;
+					}
 				}
-				else if(result === true) {
-					break;
+			}
+			bonds.push(bond = new Bond(listener, scope));
+		}
+		bond.bonds = bonds;
+		// Return the bond.
+		return bond;
+	};
+	/**
+	 * Returns a copy of the passed array.
+	 *
+	 * @param {!Array} input
+	 * @return {!Array}
+	 */
+	function copy(input) {
+		return input.slice(0);
+	}
+	/**
+	 * Emits an event. All the listeners that are registered to this event emitter for the passed event type will be notified.
+	 *
+	 * @param {!string} eventType
+	 * @return {undefined}
+	 */
+	EventEmitter.prototype["emit"] = function(eventType) {
+		// Find the bond bundle. Prefix "reserved" event types.
+		if (emptyObject[eventType]) {
+			var bonds = this.bondBundles[eventType = (reservedEventTypePrefix + eventType)];
+		} else {
+			var bonds = this.bondBundles[eventType];
+		}
+		if (bonds) {
+			if (1 == bonds.length) {
+				this.callingListener = bonds[0].callListener;
+				this.callingListener();
+			} else if (bonds.length /* To ensure not too much work is done if the bond bundle is empty. */) {
+				// Copy the bond bundle, to make sure adding and removing bonds won't cause the lines below to behave unexpectedly.
+				bonds = copy(bonds);
+				// Call the listeners of the bonds.
+				for (var index = 0; index < bonds.length; index++) {
+					this.callingListener = bonds[index].callListener;
+					this.callingListener();
+				}
+			}
+			// Delete the temporary property.
+			delete this.callingListener;
+		}
+	};
+	/**
+	 * Banana banana banana.
+	 *
+	 * @param {!string} eventType
+	 * @param {!function():*} listener
+	 * @param {*=} scope
+	 * @return {undefined}
+	 */
+	EventEmitter.prototype["remove"] = function(eventType, listener, scope) {
+		// Determine the scope that will be passed to the bond, based on the passed scope and the target passed to the constructor.
+		if (undefined === scope) {
+			// If no specific scope is defined, not in the constructor nor in this function, use null as the scope.
+			if (this == this.targetAndDefaultScope) {
+				scope = null;
+			// If no scope was defined to in this function, but a scope was defined in the constructor, use that scope.
+			} else {
+				scope = this.targetAndDefaultScope;
+			}
+		}
+		// Find the bond bundle. Prefix "reserved" event types.
+		if (emptyObject[eventType]) {
+			var bonds = this.bondBundles[eventType = (reservedEventTypePrefix + eventType)];
+		} else {
+			var bonds = this.bondBundles[eventType];
+		}
+		if (bonds) {
+			for (var index = 0; index < bonds.length; index++) {
+				if (bonds[index].listener == listener && bonds[index].originalScope === scope) {
+					bonds.splice(index, 1);
+					// As duplicates can't exist, return directly after a catch.
+					return;
 				}
 			}
 		}
-		
-		// Return the instance to allow chaining
-		return this;
 	};
-	
-	/**
-	 * Adds an event listener for the specified event
-	 * 
-	 * @param {String} type Event type name
-	 * @param {Function} listener Function to be called when the event is fired
-	 * @param {Object} scope Object that this should be set to when the listener is called
-	 * @param {Boolean} once If true then the listener will be removed after the first call
-	 * @return {Object} The current EventEmitter instance to allow chaining
-	 */
-	EventEmitter.prototype.addListener = function(type, listener, scope, once) {
-		// Create the listener array if it does not exist yet
-		if(!this._events.hasOwnProperty(type)) {
-			this._events[type] = [];
-		}
-		
-		// Push the new event to the array
-		this._events[type].push(new Event(type, listener, scope, once, this));
-		
-		// Emit the new listener event
-		this.emit('newListener', type, listener, scope, once);
-		
-		// Check if we have exceeded the maxListener count
-		// Ignore this check if the count is 0
-		// Also don't check if we have already fired a warning
-		if(this._maxListeners && !this._events[type].warned && this._events[type].length > this._maxListeners) {
-			// The max listener count has been exceeded!
-			// Warn via the console if it exists
-			if(typeof console !== 'undefined') {
-				console.warn('Possible EventEmitter memory leak detected. ' + this._events[type].length + ' listeners added. Use emitter.setMaxListeners() to increase limit.');
-			}
-			
-			// Set the flag so it doesn't fire again
-			this._events[type].warned = true;
-		}
-		
-		// Return the instance to allow chaining
-		return this;
-	};
-	
-	/**
-	 * Alias of the addListener method
-	 * 
-	 * @param {String} type Event type name
-	 * @param {Function} listener Function to be called when the event is fired
-	 * @param {Object} scope Object that this should be set to when the listener is called
-	 * @param {Boolean} once If true then the listener will be removed after the first call
-	 */
-	EventEmitter.prototype.on = EventEmitter.prototype.addListener;
-	
-	/**
-	 * Alias of the addListener method but will remove the event after the first use
-	 * 
-	 * @param {String} type Event type name
-	 * @param {Function} listener Function to be called when the event is fired
-	 * @param {Object} scope Object that this should be set to when the listener is called
-	 * @return {Object} The current EventEmitter instance to allow chaining
-	 */
-	EventEmitter.prototype.once = function(type, listener, scope) {
-		return this.addListener(type, listener, scope, true);
-	};
-	
-	/**
-	 * Removes the a listener for the specified event
-	 * 
-	 * @param {String} type Event type name the listener must have for the event to be removed
-	 * @param {Function} listener Listener the event must have to be removed
-	 * @param {Object} scope The scope the event must have to be removed
-	 * @return {Object} The current EventEmitter instance to allow chaining
-	 */
-	EventEmitter.prototype.removeListener = function(type, listener, scope) {
-		this.eachListener(type, function(currentListener, index) {
-			// If this is the listener remove it from the array
-			// We also compare the scope if it was passed
-			if(currentListener.listener === listener && (!scope || currentListener.scope === scope)) {
-				this._events[type].splice(index, 1);
-			}
-		});
-		
-		// Remove the property if there are no more listeners
-		if(this._events[type] && this._events[type].length === 0) {
-			delete this._events[type];
-		}
-		
-		// Return the instance to allow chaining
-		return this;
-	};
-	
-	/**
-	 * Alias of the removeListener method
-	 * 
-	 * @param {String} type Event type name the listener must have for the event to be removed
-	 * @param {Function} listener Listener the event must have to be removed
-	 * @param {Object} scope The scope the event must have to be removed
-	 * @return {Object} The current EventEmitter instance to allow chaining
-	 */
-	EventEmitter.prototype.off = EventEmitter.prototype.removeListener;
-	
-	/**
-	 * Removes all listeners for a specified event
-	 * If no event type is passed it will remove every listener
-	 * 
-	 * @param {String} type Event type name to remove all listeners from
-	 * @return {Object} The current EventEmitter instance to allow chaining
-	 */
-	EventEmitter.prototype.removeAllListeners = function(type) {
-		// Check for a type, if there is none remove all listeners
-		// If there is a type however, just remove the listeners for that type
-		if(type && this._events.hasOwnProperty(type)) {
-			delete this._events[type];
-		}
-		else if(!type) {
-			this._events = {};
-		}
-		
-		// Return the instance to allow chaining
-		return this;
-	};
-	
-	/**
-	 * Retrieves the array of listeners for a specified event
-	 * 
-	 * @param {String} type Event type name to return all listeners from
-	 * @return {Array} Will return either an array of listeners or an empty array if there are none
-	 */
-	EventEmitter.prototype.listeners = function(type) {
-		// Return the array of listeners or an empty array if it does not exist
-		if(this._events.hasOwnProperty(type)) {
-			// It does exist, loop over building the array
-			var listeners = [];
-			
-			this.eachListener(type, function(evt) {
-				listeners.push(evt.listener);
-			});
-			
-			return listeners;
-		}
-		
-		return [];
-	};
-	
-	/**
-	 * Emits an event executing all appropriate listeners
-	 * All values passed after the type will be passed as arguments to the listeners
-	 * 
-	 * @param {String} type Event type name to run all listeners from
-	 * @return {Object} The current EventEmitter instance to allow chaining
-	 */
-	EventEmitter.prototype.emit = function(type) {
-		// Calculate the arguments
-		var args = [],
-			i = null;
-		
-		for(i = 1; i < arguments.length; i += 1) {
-			args.push(arguments[i]);
-		}
-		
-		this.eachListener(type, function(currentListener) {
-			return currentListener.fire(args);
-		});
-		
-		// Return the instance to allow chaining
-		return this;
-	};
-	
-	/**
-	 * Sets the max listener count for the EventEmitter
-	 * When the count of listeners for an event exceeds this limit a warning will be printed
-	 * Set to 0 for no limit
-	 * 
-	 * @param {Number} maxListeners The new max listener limit
-	 * @return {Object} The current EventEmitter instance to allow chaining
-	 */
-	EventEmitter.prototype.setMaxListeners = function(maxListeners) {
-		this._maxListeners = maxListeners;
-		
-		// Return the instance to allow chaining
-		return this;
-	};
-	
-	// Export the class
-	if(typeof define === 'function' && define.amd) {
+	// TODO: export the class
+	/*if(typeof define === 'function' && define.amd) {
 		define(function() {
 			return EventEmitter;
 		});
 	}
-	else {
-		exports.EventEmitter = EventEmitter;
-	}
+	else {*/
+		exports["EventEmitter"] = EventEmitter;
+	//}
 }(this));
